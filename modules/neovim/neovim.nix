bfd78b44b23994ad039eef319abb107894de003b
migueldeoleiros <miguel.lopez@merlinsoftware.es>
2025-11-29 20:53:59 +0100

    exclude lazy-lock to try to make it work with nix

--- modules/neovim/neovim.nix
+++ modules/neovim/neovim.nix
@@ -1,5 +1,8 @@
 { config, pkgs, lib, ... }:
 
 {
-  xdg.configFile."nvim".source = ./nvim-config;
+  # Source individual files instead of the whole directory
+  # so lazy.nvim can write lazy-lock.json to ~/.config/nvim/
+  xdg.configFile."nvim/init.lua".source = ./nvim-config/init.lua;
+  xdg.configFile."nvim/lua/plugins".source = ./nvim-config/lua/plugins;
 }
