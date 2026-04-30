e91682ed9e6ed0e9e718a830b66ce9bf9a0cf86a
migueldeoleiros <miguel.lopez@merlinsoftware.es>
2026-04-30 12:49:00 +0200

    Use upstream NixOS netbird package

--- modules/nixos/netbird/netbird.nix
+++ modules/nixos/netbird/netbird.nix
@@ -1,20 +1,8 @@
-# Extend nixpkgs' netbird module with user-level systemd service
-{ config, pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
-  # Add netbird package and user service
-  environment.systemPackages = [ pkgs.netbird ];
-
-  systemd.user.services.netbird = {
-    description = "Netbird VPN";
-    # Set to [] to keep NetBird installed but require manual `systemctl --user start netbird`.
-    wantedBy = [ "default.target" ];
-    after = [ "graphical-session.target" ];
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.netbird}/bin/netbird up";
-      ExecStop = "${pkgs.netbird}/bin/netbird down";
-      Restart = "on-failure";
-    };
+  services.netbird = {
+    enable = true;
+    package = pkgs.netbird;
   };
 }
